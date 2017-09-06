0311fae65e38a437b30b6806e2a75806a3d628ad
yousif al timimie <yaltimimie@gmail.com>
2017-09-05 20:25:26 -0600

    add remove label API

--- app/toolBar/toolbar_controller.js
+++ app/toolBar/toolbar_controller.js
@@ -4,6 +4,7 @@
   .module('angular-inbox', )
   .controller('toolbarController', toolbarController)
   function toolbarController($http){
+    var url = "https://young-lake-58938.herokuapp.com/api/messages"
     const vm=this
     vm.$onInit=function(){
 
@@ -17,17 +18,16 @@
          }
          return count
        }
-
       }
+
       vm.allSelected=function (messages){
         if (messages !==undefined) {
-
           return status=messages.every(function(data){
             return data.selected == true
           })
         }
-
       }
+
       vm.someSelected=function (messages){
         if (messages !==undefined) {
           const someMsgs=messages.some(function(data){
@@ -38,7 +38,6 @@
           })
           return  someMsgs && !allMsgs
         }
-
       }
       vm.allNotSelected=function (messages){
         if (messages !== undefined) {
@@ -46,9 +45,9 @@
             return data.selected !==true
           })
         }
-
         }
     }
+
     vm.markAsRead=function(messages){
       var messageIds=[]
       for (var i = 0; i < messages.length; i++) {
@@ -62,11 +61,11 @@
         command:'read',
         read:true
       }
-      $http.patch(`https://young-lake-58938.herokuapp.com/api/messages`,JSON.stringify(body))
+      $http.patch(url,JSON.stringify(body))
         .then(function(response){
         })
-
     }
+
     vm.markAsUnRead=function(messages){
       var UnreadmessageIds=[]
       console.log(messages);
@@ -74,7 +73,6 @@
         if (messages[i].selected) {
           messages[i].read=false
           UnreadmessageIds.push(messages[i].id)
-
         }
       }
       var body={
@@ -82,41 +80,52 @@
         command:'read',
         read:false
       }
-      $http.patch(`https://young-lake-58938.herokuapp.com/api/messages`,JSON.stringify(body))
+      $http.patch(url,JSON.stringify(body))
         .then(function(response){
         })
-
     }
+
     vm.addLabel=function(messages,label){
+      var labelMessageIds=[]
       for (var i = 0; i < messages.length; i++) {
         var labelExist=messages[i].labels.includes(label)
         if (messages[i].selected && !labelExist ) {
           messages[i].labels.push(label)
-        var body={ message_id: messages[i].id,
-         label: label}
-        console.log(body);
-        $http.post(`http://localhost:3000/messages/addLabel/`,body).then(function(message){
-
-        })
+          labelMessageIds.push(messages[i].id)
         }
       }
+         var body={
+             messageIds:labelMessageIds,
+             command:'addLabel',
+             label:label
+           }
+          $http.patch(url,JSON.stringify(body))
+          .then(function(response){
+          })
     }
+
     vm.removeLabel=function(messages, label){
+      var removeLabelMsgId=[]
       for (var i = 0; i < messages.length; i++) {
         if (messages[i].selected) {
+          removeLabelMsgId.push(messages[i].id)
           var index=messages[i].labels.indexOf(label)
-          console.log(index);
           if (index > -1) {
           messages[i].labels.splice(index, 1);
-          var body1={ message_id: messages[i].id,
-           label: label}
-          console.log(body1);
-          $http.delete(`http://localhost:3000/messages/deleteLabel/${messages[i].id}/${label}`).then(function(message){
-          })
           }
         }
       }
+      var body={
+          messageIds:removeLabelMsgId,
+          command:'removeLabel',
+          label:label
+        }
+       $http.patch(url,JSON.stringify(body))
+       .then(function(response){
+         
+       })
     }
+
     vm.changeSelect=function(data){
       if (vm.allSelected(data)) {
         for (var i = 0; i < data.length; i++) {
@@ -136,7 +145,6 @@
     }
 
     vm.deleteMessage=function(messages){
-
    var deleteArr =[]
     for (var i = 0; i < messages.length; i++) {
       if (messages[i].selected) {
@@ -144,14 +152,12 @@
         messages.splice(i, 1);
         i--
       }
-
     }
     var body={
       messageIds:deleteArr,
       command:'delete'
-
     }
-    $http.patch(`https://young-lake-58938.herokuapp.com/api/messages`,JSON.stringify(body))
+    $http.patch(url,JSON.stringify(body))
       .then(function(response){
       })
     }
