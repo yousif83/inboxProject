3a74ed2b35d2473b385691c97460ac994ee210e9
yousif al timimie <yaltimimie@gmail.com>
2017-09-07 09:25:20 -0600

    commit

--- app/composeForm/composeForm_controller.js
+++ app/composeForm/composeForm_controller.js
@@ -7,7 +7,6 @@
        var url = "https://young-lake-58938.herokuapp.com/api/messages"
      const vm=this
     vm.$onInit=function(){
-      console.log(vm);
     }
     vm.addMessage=function(messages,txtBody,txtSubject,hidefrm){
       hidefrm.flag=true
@@ -15,10 +14,9 @@
         subject:txtSubject,
         body:txtBody
       }
-      console.log(mainBody);
+
       $http.post(url,JSON.stringify(mainBody))
       .then(function(response){
-       console.log(response.data.body);
        messages.push(response.data)
       })
     }
